181e5345d27be1f2a68027af680bf526e6869fe1
LeoKnox <LeoKnox@users.noreply.github.com>
2024-06-06 13:59:46 -0700

    Update Build.js

--- Build.js
+++ Build.js
@@ -5,19 +5,21 @@ export default Build = () => {
     <>
       <h3>Build Page</h3>;
       <Canvas camera={{ position: [3, 3, 1] }}>
+        <pointLight position={[2, 2, 2]} />
+        <ambientLight />
         <group>
-        <mesh position={[1, 0, 0]}>
-          <boxGeometry />
-          <meshStandardMaterial />
-        </mesh>
-        <mesh position={[-1, 0, 0]}>
-          <boxGeometry />
-          <meshStandardMaterial />
-        </mesh>
-        <mesh position={[0, -1, 0]}>
-          <planeGeometry attach="geometry" args={[25, 15]} />
-          <meshStandardMaterial color={"red"} />
-        </mesh>
+          <mesh position={[1, 0, 0]}>
+            <boxGeometry />
+            <meshStandardMaterial />
+          </mesh>
+          <mesh position={[-1, 0, 0]}>
+            <boxGeometry />
+            <meshStandardMaterial />
+          </mesh>
+          <mesh position={[0, -3, 0]}>
+            <planeGeometry attach="geometry" args={[25, 8]} />
+            <meshStandardMaterial color = "red" />
+          </mesh>
         </group>
       </Canvas>
       ;
